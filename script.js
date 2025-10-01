095ba777bcf919116bf9c50fb9a83af3c8a18b05
antonioroserom <wrosero9059@utm.edu.ec>
2025-09-30 21:14:51 -0500

    Verificamos si es un número o punto decimal, si aún no llega al máximo de dígitos solo permite 8 para cada operando y se no se permite ingresar dos operadores a la vez

--- script.js
+++ script.js
@@ -6,5 +6,25 @@ const MAX_DIGITOS = 8;
 
 function agregar(valor) {
     const operadores = "+-*/%";
-    
+
+    // Verifica si es número o punto decimal
+    if ("0123456789.".includes(valor)) {
+        // Divide la expresión en pantalla en partes separadas por operadores
+        const partes = pantalla.value.split(/[\+\-\*\/%]/);
+        const ultimoNumero = partes[partes.length -1];
+
+        // Permite agregar un último número si no excede el máximo permitido
+        if (ultimoNumero.length < MAX_DIGITOS) {
+            pantalla.value += valor;
+        }
+    } else {
+        // No permite dos operadores
+        const ultimo = pantalla.value.slice(-1);
+        if (operadores.includes(valor) && !operadores.includes(ultimo) && pantalla.value !== "") {
+
+        }
+    }
 }
+
+
+
